0dbc11935c4bee6ebaaace549de0721e420aaabe
Marcos1Guerra <marcos.guerra@ges.inatel.br>
2026-03-09 18:43:52 -0300

    desafio-qa-beedoo

--- desafio-qa-beedoo/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add('preencherFormularioBase', (dados) => {
-  cy.get('input[aria-label="Nome do curso"]').type(dados.nome);
-  cy.get('textarea[aria-label="Descrição do curso"]').type(dados.descricao);
-  cy.get('input[aria-label="Instrutor"]').type(dados.instrutor);
-  cy.get('input[aria-label="Url da imagem de capa"]').type(dados.url);
-  cy.get('input[aria-label="Data de início"]').type(dados.inicio);
-  cy.get('input[aria-label="Data de fim"]').type(dados.fim);
-  cy.get('input[aria-label="Número de vagas"]').type(dados.vagas);
-});
